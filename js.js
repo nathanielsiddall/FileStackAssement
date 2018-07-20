ac6f253e537faf4b2a4a145ab8f64c345d1fe15a
nathanielsiddall <nathaniel.siddall@gmail.com>
2018-07-20 16:03:40 -0500

    Function: hid the file uploader behind the file uploade button.

--- js.js
+++ js.js
@@ -3,49 +3,34 @@ const resultsData = {
     name: "",
     imgURL: ""
 };
-const client = filestack.init("AmzTJqxaPT1Ch6kK4YvKsz");
-const listBox = document.getElementById('listBox');
 
+const client = filestack.init("AmzTJqxaPT1Ch6kK4YvKsz");
+
+ function picker() {
 
 client.pick({maxFiles: 5})
-    .then( (results)=>{
-    // console.log(JSON.stringify(results));
+    .then( (results)=> {
         console.log(results);
         console.log(results.filesUploaded.length);
         let output = "";
 
-        for(let i = 0; i < results.filesUploaded.length; i++){
-            resultsData.name = results.filesUploaded[i].filename;
-            resultsData.imgURL = results.filesUploaded[i].url;
+            for(let i = 0; i < results.filesUploaded.length; i++){
+                resultsData.name = results.filesUploaded[i].filename;
+                resultsData.imgURL = results.filesUploaded[i].url;
 
-            console.log(resultsData.name);
-            console.log(resultsData.imgURL);
+                console.log(resultsData.name);
+                console.log(resultsData.imgURL);
 
-            output +=  "<a href=\"" + results.filesUploaded[i].url+ "\">" + results.filesUploaded[i].filename +  "</a> \n";
-        listBox.innerHTML = output;
+                output +=  "<a href=\"" + results.filesUploaded[i].url+ "\">" + results.filesUploaded[i].filename +  "</a> \n";
+            listBox.innerHTML = output;
+        }});}
+
+const listBox = document.getElementById('listBox');
+const pickerButton = document.getElementById('pickerButton');
+
+pickerButton.addEventListener("click", picker, false);
 
 
 
-    }});
 
 
-function updateForm (result) {
-    const fileData = result.filesUploaded[0];
-    fileInput.value = fileData.url;
-
-    // If file is resizable image, resize and embed it as a thumbnail preview
-    if (['jpeg', 'png'].indexOf(fileData.mimetype.split('/')[1]) !== -1) {
-        const container = document.getElementById('thumbnail-container');
-        const thumbnail = document.getElementById('thumbnail') || new Image();
-        thumbnail.id = 'thumbnail';
-        thumbnail.src = client.transform(fileData.handle, {
-            resize: {
-                width: 200
-            }
-        });
-
-        if (!container.contains(thumbnail)) {
-            container.appendChild(thumbnail);
-        }
-    }
-};
