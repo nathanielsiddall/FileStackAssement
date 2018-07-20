42c412fc87ee6918339c5c6c38580be5c0c21df8
nathanielsiddall <nathaniel.siddall@gmail.com>
2018-07-20 18:34:17 -0500

    evening commit.

--- js.js
+++ js.js
@@ -1,10 +1,7 @@
 
 const client = filestack.init("AmzTJqxaPT1Ch6kK4YvKsz");
 
-
-let containerBox = "";
 const container = document.getElementById('listBox');
-const thumbnail = document.getElementById('thumbnail') || new Image();
 
     const displayUploads = (results)=>{
 
@@ -13,36 +10,51 @@ const thumbnail = document.getElementById('thumbnail') || new Image();
         const fileData = results.filesUploaded[i];
         console.log(fileData);
 
+
+        //todo: the options are not valid. figure out how to transform documents and make the thumbnail tab work with them.
+            let options = {"output=format":"png"};
+            client.transform(fileData.url, options);
+
         if (['jpeg', 'png'].indexOf(fileData.mimetype.split('/')[1]) !== -1) {
-            container.innerHTML += "<a href=\""+ fileData.url +"\">" +fileData.filename +"<img src=\""+ fileData.url +"\" height=\"100\" width=\"150\"></a> \n"
-        }
+            container.innerHTML += "<a href=\""+ fileData.url +"\" target=\"_blank\">" +fileData.filename +"<img src=\""+ fileData.url +"\" height=\"100\" width=\"150\"></a>"
+        }else container.innerHTML += "<a href=\""+ fileData.url +"\" target=\"_blank\">" +fileData.filename + "</a>"
     }};
 
     const picker = ()=> {
 
-     client.pick({maxFiles: 5})
+     client.pick({maxFiles: 5, })
     .then( (results)=> {
         console.log(results);
         console.log(results.filesUploaded.length);
-        // let output = "";
         displayUploads(results);
-
-
-        //     for(let i = 0; i < results.filesUploaded.length; i++){
-        //         resultsData.name = results.filesUploaded[i].filename;
-        //         resultsData.imgURL = results.filesUploaded[i].url;
-        //
-        //         console.log(resultsData.name);
-        //         console.log(resultsData.imgURL);
-        //
-        //         output +=  "<a href=\"" + results.filesUploaded[i].url+ "\">" + results.filesUploaded[i].filename +  "</a> \n";
-        //     listBox.innerHTML = output;
     })};
 
-const listBox = document.getElementById('listBox');
+
+
 const pickerButton = document.getElementById('pickerButton');
 
 pickerButton.addEventListener("click", picker, false);
+
+
+//heres a copy of the specs
+
+/*
+Assignment: Create file uploader and display file list
+1. Required: Please integrate Filestack Picker (1.0.x version of our JS SDK https://github.com/filestack/filestack-js) on an HTML page that will allow you to upload any file (or multiple files - max 5). The list of uploaded files should be displayed on the page after the Picker is closed, this should be comprised of the file uploaded in accessible format (showing a thumbnail of an image uploaded for example, like explained below).
+No backend is required to keep a list of uploaded files. You can display only files that were uploaded in the given Picker session, not files from other sessions.
+2. Required: For each file:
+- if it is an image a thumbnail should also be displayed next to the file name and both file name and thumbnail should be clickable and open a new tab
+with the image.
+- if it is a document a thumbnail of the first page should be displayed next to the file name and both file name and thumbnail should be clickable and
+open new tab with document Preview.
+- if it is not image or document (supported by our transformations) no thumbnail is displayed and just file name links to the file download
+3. Optional: Configure simple NodeJS HTTP service that will host HTML page you created in steps 1 and 2.
+4. Optional: Display files that were uploaded in the previous sessions of the picker. You will need some kind of database to store information about files you have uploaded previously in order to display them.
+You can sign up for a free account here: https://dev.filestack.com/signup/trial/
+And access our docs here: https://www.filestack.com/docs
+*/
+
+
 
 
 
