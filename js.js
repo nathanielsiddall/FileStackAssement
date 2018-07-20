fc9751cb0f982095ad2f7452817cd372ced9473a
nathanielsiddall <nathaniel.siddall@gmail.com>
2018-07-20 17:49:52 -0500

    Function: now able to list uploaded images.

--- js.js
+++ js.js
@@ -1,29 +1,43 @@
 
-const resultsData = {
-    name: "",
-    imgURL: ""
-};
-
 const client = filestack.init("AmzTJqxaPT1Ch6kK4YvKsz");
 
- function picker() {
 
-client.pick({maxFiles: 5})
+let containerBox = "";
+const container = document.getElementById('listBox');
+const thumbnail = document.getElementById('thumbnail') || new Image();
+
+    const displayUploads = (results)=>{
+
+        for (let i = 0; i < results.filesUploaded.length; i++){
+
+        const fileData = results.filesUploaded[i];
+        console.log(fileData);
+
+        if (['jpeg', 'png'].indexOf(fileData.mimetype.split('/')[1]) !== -1) {
+            container.innerHTML += "<a href=\""+ fileData.url +"\">" +fileData.filename +"<img src=\""+ fileData.url +"\" height=\"100\" width=\"150\"></a> \n"
+        }
+    }};
+
+    const picker = ()=> {
+
+     client.pick({maxFiles: 5})
     .then( (results)=> {
         console.log(results);
         console.log(results.filesUploaded.length);
-        let output = "";
+        // let output = "";
+        displayUploads(results);
 
-            for(let i = 0; i < results.filesUploaded.length; i++){
-                resultsData.name = results.filesUploaded[i].filename;
-                resultsData.imgURL = results.filesUploaded[i].url;
 
-                console.log(resultsData.name);
-                console.log(resultsData.imgURL);
-
-                output +=  "<a href=\"" + results.filesUploaded[i].url+ "\">" + results.filesUploaded[i].filename +  "</a> \n";
-            listBox.innerHTML = output;
-        }});}
+        //     for(let i = 0; i < results.filesUploaded.length; i++){
+        //         resultsData.name = results.filesUploaded[i].filename;
+        //         resultsData.imgURL = results.filesUploaded[i].url;
+        //
+        //         console.log(resultsData.name);
+        //         console.log(resultsData.imgURL);
+        //
+        //         output +=  "<a href=\"" + results.filesUploaded[i].url+ "\">" + results.filesUploaded[i].filename +  "</a> \n";
+        //     listBox.innerHTML = output;
+    })};
 
 const listBox = document.getElementById('listBox');
 const pickerButton = document.getElementById('pickerButton');
